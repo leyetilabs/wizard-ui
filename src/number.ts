585d52588d5b45dba59c16d61e844f05654ad627
Arthur <itey.arthur@gmail.com>
2021-11-16 15:50:58 +0800

    fix(number): change type for value

--- src/number.ts
+++ src/number.ts
@@ -15,25 +15,28 @@ BigNumber.config({
  * @returns string
  */
 export const fromTerraAmount = (
-  value: string = '0',
+  value: BigNumber.Value = '0',
   format = '0,0.00a',
 ): string => {
   const amount = new BigNumber(value).div(ONE_TOKEN)
   return numeral(amount).format(format).toUpperCase()
 }
 
-export const toTerraAmount = (value: string = '0'): string => {
+export const toTerraAmount = (value: BigNumber.Value = '0'): string => {
   return new BigNumber(value).dp(6).times(ONE_TOKEN).toString()
 }
 
-export const toDecimal = (value: string = '0', dp: number = 6): string => {
+export const toDecimal = (
+  value: BigNumber.Value = '0',
+  dp: number = 6,
+): string => {
   return new BigNumber(value).toFixed(dp).toString()
 }
 
-export const toNumber = (value: string = '0'): number => {
+export const toNumber = (value: BigNumber.Value = '0'): number => {
   return new BigNumber(value).toNumber()
 }
 
-export const num = (value: string = '0'): BigNumber => {
+export const num = (value: BigNumber.Value = '0'): BigNumber => {
   return new BigNumber(value)
 }
