375c74487946e353dce5a7f889d123ea64753c2c
Arthur Itey <itey.arthur@gmail.com>
2022-08-14 12:46:09 +0200

    Fix start application

--- apps/starter/next.config.js
+++ apps/starter/next.config.js
@@ -1,4 +1,8 @@
-const withTM = require("next-transpile-modules")(["d3-format"]);
+const withTM = require("next-transpile-modules")([
+  "d3-format",
+  "@wizard-ui/core",
+  "@wizard-ui/react",
+]);
 
 /** @type {import('next').NextConfig} */
 const config = {
